f4e3be0c657a50582c929dc8b6f0bfc3a3646a65
Egor Zhelobanov <e.zhelobanov@gmail.com>
2016-12-16 17:09:57 +1100

    Base...

---
+++ src/resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.main')
+
+@section('body-title')
+    {{ trans('blog::blog.index.title') }}
+@endsection
+
+@section('body')
+    <div class="blog-index">
+
+    </div>
+@endsection
